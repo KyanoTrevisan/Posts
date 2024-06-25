1f467c94233c16e0c58c495a55635aba9cd3ca6a
KyanoTrevisan <r0894962@student.thomasmore.be>
2024-06-25 22:07:34 +0200

    added tests because they suddenly work again no actually because i fixed it on purpose ...i swear!

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -2,6 +2,16 @@
 
 use App\Models\User;
 
+test('profile page is displayed', function () {
+    $user = User::factory()->create();
+
+    $response = $this
+        ->actingAs($user)
+        ->get('/profile');
+
+    $response->assertOk();
+});
+
 test('profile information can be updated', function () {
     $user = User::factory()->create();
 
@@ -38,6 +48,22 @@ test('email verification status is unchanged when the email address is unchanged
         ->assertRedirect('/profile');
 
     $this->assertNotNull($user->refresh()->email_verified_at);
+});
+
+test('user can delete their account', function () {
+    $user = User::factory()->create([
+        'password' => bcrypt('password')
+    ]);
+
+    $this->actingAs($user)
+        ->delete('/profile', [
+            'password' => 'password',
+        ])
+        ->assertSessionHasNoErrors()
+        ->assertRedirect('/');
+
+    $this->assertGuest();
+    $this->assertNotNull($user->fresh()->deleted_at);
 });
 
 test('correct password must be provided to delete account', function () {
